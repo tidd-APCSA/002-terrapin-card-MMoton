a9c349527d525ce155e2a18981165ab75cab3e08
maya moton <mayam03@icloud.com>
2020-10-22 15:13:10 +0000

    I changed "."

--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,38 @@
 public class TerrapinCard {
+  private double balance;
+
+  public TerrapinCard(double balance){
+    this.balance = balance;
+  }
+
+  public void payEconomical(){
+
+    if(this.balance >= 2.50){
+      this.balance = this.balance - 2.50;
+    }
+  }
+  
+  public void payGourmet(){
+        if(this.balance >= 4){
+      this.balance = this.balance - 4;
+    }
+  }
+
+  public void loadMoney(double amount){
+    if(amount >= 0){
+      this.balance = this.balance + amount;
+    }
+    if(this.balance >= 150){
+      this.balance = 150;
+    }
+  }
+
+public double getBalance(){
+  return this.balance;
+}
 
 
+  public String toString(){
+    return "The card has " + this.balance + " dollars";
+  }
 }
