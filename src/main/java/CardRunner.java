4286d14c806ae32dc873d3acc46e997b13b04e82
maya moton <mayam03@icloud.com>
2020-10-22 14:33:05 +0000

    submitting

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,34 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        System.out.println("hey");
+        TerrapinCard maya = new TerrapinCard(5);
+        TerrapinCard cardBilly = new TerrapinCard(20);
+        TerrapinCard cardBrian = new TerrapinCard(30);
 
+      cardBilly.payGourmet();
+      cardBrian.payEconomical();
+      System.out.println("Billy: " + cardBilly);
+      System.out.println("Brian: " + cardBrian);
+      cardBilly.loadMoney(20);
+      cardBrian.payGourmet();
+      System.out.println("Billy: " + cardBilly);
+      System.out.println("Brian: " + cardBrian);
+      cardBilly.payEconomical();
+      cardBrian.payEconomical();
+      cardBrian.loadMoney(50);
+      System.out.println("Billy: " + cardBilly);
+      System.out.println("Brian: " + cardBrian);
+
+
+
+
+        System.out.println(maya);
+        maya.payEconomical();
+        System.out.println(maya);
+        maya.payGourmet();
+        System.out.println(maya);
+        maya.loadMoney(100);
+        System.out.println(maya);
+        System.out.println(maya.getBalance());
     }
 }
